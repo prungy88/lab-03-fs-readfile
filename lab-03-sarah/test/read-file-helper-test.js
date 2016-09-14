b73b240917b5a417d6e7b3eeb7f552c0b2830e5e
Sarah DeBey <sarah.debey@gmail.com>
2016-09-14 16:59:21 -0700

    asynchronous and callbacks

---
+++ lab-03-sarah/test/read-file-helper-test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const readFunctions = require('../lib/read-file-helper.js');
+const assert = require('assert');
+
+describe('testing read-file-helper', function() {
+  //the done makes the it block asynchronous, so the it block doesn't run until the done is called
+  it('hex codes should console.log in order', function(done) {
+    readFunctions(function(hexArray) {
+      assert.equal(hexArray.join(), ['46', '53', '54'].join());
+      done();
+    });
+  });
+});
+//
+// readFileHelper(function(hexArray){
+//   console.log('hexArray', hexArray);
+//   // assert goes here
+// });
